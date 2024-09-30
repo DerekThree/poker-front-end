79950a7e50bfd9d203d80a36b1f9ebc9e4aaa5b8
Derek Ochal <derek.ochal@westernunion.com>
2024-09-29 20:58:20 -0500

    Updated http call

--- poker-front-end/src/app/app.component.ts
+++ poker-front-end/src/app/app.component.ts
@@ -1,29 +1,39 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  title = 'poker-front-end';
+  title = 'Poker Odds Calculator';
   response: string = "";
+  cards = [
+    // replace suit descriptions with their symbols
+    { suit: '♠', value: 'A' },
+    { suit: '♦', value: '10' },
+    { suit: '♣', value: 'K' },
+    { suit: '♠', value: 'Q' },
+    { suit: '♥', value: 'J' }
+  ];
 
   constructor(private http: HttpClient) {}
 
   sendRequest() {
-    this.http.get('http://localhost:8080/v1/poker/health', { responseType: 'text' }).subscribe((res: string) => {
-      this.response = res;
-    });
+    this.http.post('http://localhost:8080/v1/poker/hand', this.cards, { responseType: 'text' })
+      .subscribe((res: string) => {
+        this.response = res;
+        console.log("Response: " + res);
+      });
   }
 
-
-  
 
   
 }
