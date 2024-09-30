e9c09a7c8340c43fa4547009d4c74e1764938b80
Derek Ochal <derek.ochal@westernunion.com>
2024-09-29 21:24:09 -0500

    Added dropdowns

--- poker-front-end/src/app/app.component.ts
+++ poker-front-end/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
 import { CommonModule } from "@angular/common";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, CommonModule],
+  imports: [RouterOutlet, CardComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,13 +17,15 @@ export class AppComponent {
   title = 'Poker Odds Calculator';
   response: string = "";
   cards = [
-    // replace suit descriptions with their symbols
     { suit: '♠', value: 'A' },
     { suit: '♦', value: '10' },
     { suit: '♣', value: 'K' },
     { suit: '♠', value: 'Q' },
     { suit: '♥', value: 'J' }
   ];
+
+  suits = ['♥', '♠', '♦', '♣'];
+  values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
   constructor(private http: HttpClient) {}
 
