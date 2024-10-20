02775348dc6369a933e8d1bae7ab3ad4942f4100
Derek Ochal <derek.ochal@westernunion.com>
2024-10-20 15:50:21 -0500

    Implemented routing

---
+++ poker-front-end/src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterOutlet, CardComponent, CommonModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  title = 'Poker Odds Calculator';
+  response: string = "";
+  cards = [
+    { suit: '♠', value: 'A' },
+    { suit: '♦', value: '10' },
+    { suit: '♣', value: 'K' },
+    { suit: '♠', value: 'Q' },
+    { suit: '♥', value: 'J' }
+  ];
+
+  suits = ['♥', '♠', '♦', '♣'];
+  values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  constructor(private http: HttpClient) {}
+
+  sendRequest() {
+    this.http.post('http://localhost:8080/v1/poker/hand', this.cards, { responseType: 'text' })
+      .subscribe((res: string) => {
+        this.response = res;
+        console.log("Response: " + res);
+      });
+  }
+
+
+  
+}
